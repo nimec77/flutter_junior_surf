16d78c22fa6db3c989eb5cc9f20d03d795ba3080
comrade77 <nimec77@gmail.com>
2021-10-11 16:59:44 +0700

    Разбираюсь с users_page title

--- lib/users/presentation/pages/users_page.dart
+++ lib/users/presentation/pages/users_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_junior_surf/login/presentation/blocs/auth_bloc.dart';
+import 'package:sizer/sizer.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({Key? key, required this.authBloc}) : super(key: key);
@@ -9,17 +12,40 @@ class UsersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('Users Page', style: Theme.of(context).textTheme.headline3),
-            ElevatedButton(
-              onPressed: () => authBloc.add(const AuthEvent.logout()),
-              child: const Text('Logout'),
+      // backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            pinned: true,
+            expandedHeight: 15.h,
+            flexibleSpace: const FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              centerTitle: true,
+              title: Text(
+                'Пользователи',
+                style: TextStyle(color: Colors.black),
+              ),
             ),
-          ],
-        ),
+            onStretchTrigger: () async {
+              debugPrint('1');
+            },
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+        ],
       ),
     );
   }
--- test/helpers/user_helper.dart
+++ test/helpers/user_helper.dart
@@ -8,27 +8,26 @@ class UserHelper {
   UserHelper({int? seed}) : faker = Faker(seed: seed);
 
   final Faker faker;
-  int _id = 1;
+  int _id = 0;
+
+  String get randomGeo =>
+      ((faker.randomGenerator.boolean() ? 1 : -1) * faker.randomGenerator.decimal()).toStringAsFixed(4);
 
   UserPod fakeUserPod() {
-    final geo = Geo(
-      lat: faker.randomGenerator.decimal().toString(),
-      lng: faker.randomGenerator.decimal().toString(),
-    );
     final address = Address(
       street: faker.address.streetName(),
       suite: faker.address.streetAddress(),
       city: faker.address.city(),
       zipcode: faker.address.zipCode(),
-      geo: geo,
+      geo: Geo(lat: randomGeo, lng: randomGeo),
     );
     final company = Company(
       name: faker.company.name(),
-      catchPhrase: faker.company.position(),
-      bs: faker.company.suffix(),
+      catchPhrase: faker.lorem.sentence(),
+      bs: faker.company.position(),
     );
     return UserPod(
-      id: _id++,
+      id: ++_id,
       username: faker.person.name(),
       email: faker.internet.email(),
       address: address,
@@ -39,6 +38,6 @@ class UserHelper {
   }
 
   List<UserPod> fakeUsersPod(int length) {
-    return List.generate(length, (_) => fakeUserPod());
+    return List.filled(length, fakeUserPod());
   }
 }
